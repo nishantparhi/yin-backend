b208c59a9ccf546263bc6f6e12a23056572e1b86
SANDIP SADHUKHAN <sandippakhanna@gmail.com>
2020-10-25 09:05:14 +0530

    Add sign up

--- youth_news/website/views.py
+++ youth_news/website/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm
+from django.contrib.auth import authenticate, login
 
 def index(request):
     return render(request, 'website/index.html')
     
+
+def register(request):
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+
+    form = CreateUserForm()
+    context = {'form': form}
+    return render(request, 'registration/signup.html', context)
