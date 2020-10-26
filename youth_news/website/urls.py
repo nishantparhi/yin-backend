3ab203cd3a109bd596a3745c141432ada14eaed5
SANDIP SADHUKHAN <sandippakhanna@gmail.com>
2020-10-26 20:09:32 +0530

    Add forget password

--- youth_news/website/urls.py
+++ youth_news/website/urls.py
@@ -1,8 +1,37 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name='index'),    
-    path('register/',views.register, name='register'),
     path('single/',views.single,name="single"),
+
+    # Registration
+    path('register/',views.register, name='register'),
+
+    # Forget Password
+    path(
+        'reset_password/',
+        auth_views.PasswordResetView.as_view(template_name="registration/reset_password.html"),
+        name="reset_password"
+    ),
+
+    path(
+        'reset_password_sent/', 
+        auth_views.PasswordResetDoneView.as_view(), 
+        name="password_reset_done"
+    ),
+
+    path(
+        'reset/<uidb64>/<token>/', 
+        auth_views.PasswordResetConfirmView.as_view(), 
+        name="password_reset_confirm"
+    ),
+
+    path(
+        'reset_password_complete/', 
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete"
+    ),
+
 ]
--- youth_news/youth_news/settings.py
+++ youth_news/youth_news/settings.py
@@ -110,3 +110,12 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+# SMTP Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True 
+EMAIL_HOST_USER = 'yourmail@email.com'
+EMAIL_HOST_PASSWORD = 'your password'
+
