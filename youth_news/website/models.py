cad2fd3a49d32da305a6b61a19563baf4cb50da2
SANDIP SADHUKHAN <sandippakhanna@gmail.com>
2020-10-29 12:22:13 +0530

    Add blog-post model

--- youth_news/website/models.py
+++ youth_news/website/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import pre_save
+from .util import unique_slug_generator
 
-# Create your models here.
+# Contact Form 
 class Contact(models.Model):
     name = models.CharField(max_length=200, help_text="Name of the sender" )
     email = models.EmailField(max_length=200)
@@ -14,3 +17,22 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name + "-" +  self.email
+
+# Blog Post
+class BlogPost(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    blog_title = models.CharField(max_length=400, null=False)
+
+    slug = models.SlugField(max_length=250, null=True, blank=True)
+    blog_content = models.TextField()
+
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.blog_title
+
+def slug_generator(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+    
+pre_save.connect(slug_generator, sender=BlogPost)
