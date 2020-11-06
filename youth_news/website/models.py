10699491696a130d0c92573e200216aa06e86550
SANDIP SADHUKHAN <sandippakhanna@gmail.com>
2020-11-06 20:55:15 +0530

    add catagory tranding

--- youth_news/website/models.py
+++ youth_news/website/models.py
@@ -29,6 +29,7 @@ class Tag(models.Model):
 # Tag model
 class Catagory(models.Model):
     text = models.CharField(max_length=200)
+    tranding = models.BooleanField(default=False)
     def __str__(self):
         return self.text
 
