add4b017d332ed8da01f11cc0c6f09c3554f262d
Manasi <mansi.anantpurkar@gmail.com>
2020-11-11 23:45:20 +0530

    Login Page updated

--- youth_news/website/urls.py
+++ youth_news/website/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
     path('contact-us/', views.contactPage, name="contact"),
     path('blog-catagory/<str:catagory>/', views.blogCatagory, name="blog-catagory"),
     # Dashboard
--- youth_news/website/views.py
+++ youth_news/website/views.py
@@ -7,6 +7,8 @@ from .decorators import unauthentiated_user, notDeveloper, onlyDeveloper
 from django.contrib.auth.models import User, Group
 from django.core.paginator import Paginator
 
+def login(request):
+    return render(request, 'website/login.html')
 
 def index(request):
     trandingBlogs = BlogPost.objects.filter(
